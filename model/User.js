37b29426ca427dbac303dd77b44ff67d8eab9ac3
ke-saad <kellalisaad@gmail.com>
2024-05-21 20:50:43 +0100

    modified:   model/User.js

--- model/User.js
+++ model/User.js
@@ -1,14 +1,37 @@
-const mongoose = require("mongoose");
+// server/model/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
-const userSchema = new mongoose.Schema(
-  {
-    username: { type: String, required: true },
-    password: { type: String, required: true },
-    isAdmin: { type: Boolean, default: false },
-    timestamp: { type: Date, default: Date.now }
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
   },
-  { collection: "users" }
-);
+  password: {
+    type: String,
+    required: true,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/.test(v);
+      },
+      message: props => `${props.value} is not a valid password!`
+    }
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false,
+  }
+});
 
-const User = mongoose.model("User", userSchema);
+userSchema.pre('save', async function(next) {
+  if (this.isModified('password')) {
+    this.password = await bcrypt.hash(this.password, 10);
+  }
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
 module.exports = User;
